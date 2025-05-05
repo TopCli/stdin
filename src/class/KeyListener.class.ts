fdad5f29a9924cbc376c7c4b5cb773d96790aa35
fraxken <gentilhomme.thomas@gmail.com>
2025-05-05 15:13:43 +0200

    refactor: implement KeyListener class

---
+++ src/class/KeyListener.class.ts
@@ -0,0 +1,147 @@
+// Import Node.js Dependencies
+import * as TTY from "node:tty";
+
+// Import Internal Dependencies
+import { History } from "./History.class.js";
+import { Completion } from "./Completion.class.js";
+import { InputBuffer } from "./InputBuffer.class.js";
+
+type Key = {
+  sequence?: string;
+  name: string;
+  ctrl?: boolean;
+  meta?: boolean;
+  shift?: boolean;
+};
+
+export interface KeyListenerOptions {
+  history?: string[];
+  autocomplete?: string[];
+}
+
+export class KeyListener {
+  private history: History;
+  private autocomplete: Completion;
+  private inputBuffer: InputBuffer;
+  private output: TTY.WriteStream;
+
+  constructor(
+    options: KeyListenerOptions = {},
+    output: TTY.WriteStream = process.stdout
+  ) {
+    const {
+      autocomplete = [],
+      history
+    } = options;
+
+    this.output = output;
+    this.history = new History(history);
+    this.autocomplete = new Completion(output, autocomplete);
+    this.inputBuffer = new InputBuffer(output);
+  }
+
+  private complete() {
+    if (this.autocomplete.hint.length === 0) {
+      return;
+    }
+
+    this.inputBuffer.append(
+      this.autocomplete.hint.strip()
+    );
+    this.autocomplete.findHint(
+      this.inputBuffer.toString(),
+      { forceNextMatch: true }
+    );
+  }
+
+  private historize(
+    direction: "up" | "down"
+  ) {
+    if (this.history.length === 0) {
+      this.history.keep(this.inputBuffer.toString());
+
+      return;
+    }
+    if (
+      !this.history[direction]()
+    ) {
+      return;
+    }
+
+    this.autocomplete.clearHint();
+    this.history.keep(
+      this.inputBuffer.replace(this.history.current, { clearOutput: true })
+    );
+  }
+
+  private backspace() {
+    this.autocomplete.clearHint();
+    const { autocomplete } = this.inputBuffer.removeChar();
+
+    if (autocomplete) {
+      this.autocomplete.findHint(this.inputBuffer.toString());
+    }
+  }
+
+  createListener(
+    exitCallback: (result: string) => void
+  ) {
+    return (input: string, key: Key) => {
+      const currentText = this.inputBuffer.toString();
+      this.inputBuffer.cursor.offset = currentText.length;
+
+      switch (normalizeKey(key)) {
+        case "ctrl+c":
+        case "return":
+        case "escape":
+          exitCallback(
+            this.history.push(currentText)
+          );
+          break;
+        case "left": {
+          this.inputBuffer.cursor.left();
+          break;
+        }
+        case "right":
+        case "tab": {
+          if (key.name !== "tab" && this.inputBuffer.cursor.right()) {
+            return;
+          }
+
+          this.complete();
+          break;
+        }
+        case "up":
+        case "down": {
+          this.historize(key.name as "up" | "down");
+          break;
+        }
+        case "backspace": {
+          this.backspace();
+          break;
+        }
+        default: {
+          const { autocomplete } = this.inputBuffer.appendChar(input);
+
+          if (
+            autocomplete &&
+            this.autocomplete.findHint(this.inputBuffer.toString(), { currentInput: input })
+          ) {
+            this.output.write(input);
+          }
+          break;
+        }
+      }
+    };
+  }
+}
+
+function normalizeKey(
+  key: Key
+): string {
+  if (key.ctrl && key.name === "c") {
+    return "ctrl+c";
+  }
+
+  return key.name;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,12 @@
 import { emitKeypressEvents } from "node:readline";
 
 // Import Internal Dependencies
-import { History } from "./class/History.class.js";
-import { InputBuffer } from "./class/InputBuffer.class.js";
-import { Completion } from "./class/Completion.class.js";
+import {
+  KeyListener,
+  type KeyListenerOptions
+} from "./class/KeyListener.class.js";
 
-type Key = {
-  sequence?: string;
-  name: string;
-  ctrl?: boolean;
-  meta?: boolean;
-  shift?: boolean;
-};
-
-export interface StdinOptions {
-  history?: string[];
-  autocomplete?: string[];
-}
+export type StdinOptions = KeyListenerOptions;
 
 export default function stdin(
   title: string | null,
@@ -36,114 +26,16 @@ export default function stdin(
 
   const { resolve, promise } = Promise.withResolvers<string>();
 
-  const listener = keyPressListener(
-    new History(options.history),
-    new Completion(
-      process.stdout,
-      options?.autocomplete ?? []
-    ),
-    (result) => {
+  const listener = new KeyListener(options)
+    .createListener((result) => {
       process.stdin.setRawMode(false);
       process.stdin.pause();
       process.stdout.write("\n");
       process.stdin.removeListener("keypress", listener);
 
       resolve(result);
-    }
-  );
+    });
   process.stdin.on("keypress", listener);
 
   return promise;
-}
-
-function keyPressListener(
-  history: History,
-  autocompletion: Completion,
-  completeCallback: (result: string) => void
-) {
-  const inputBuffer = new InputBuffer(process.stdout);
-
-  return (input: string, key: Key) => {
-    inputBuffer.cursor.offset = inputBuffer.toString().length;
-
-    switch (normalizeKey(key)) {
-      case "ctrl+c":
-      case "return":
-      case "escape":
-        completeCallback(
-          history.push(inputBuffer.toString())
-        );
-        break;
-      case "left": {
-        inputBuffer.cursor.left();
-        break;
-      }
-      case "right":
-      case "tab": {
-        if (
-          (key.name !== "tab" && inputBuffer.cursor.right()) ||
-          autocompletion.hint.length === 0
-        ) {
-          break;
-        }
-
-        inputBuffer.append(
-          autocompletion.hint.strip()
-        );
-        autocompletion.findHint(
-          inputBuffer.toString(),
-          { forceNextMatch: true }
-        );
-        break;
-      }
-      case "up":
-      case "down": {
-        if (history.length === 0) {
-          history.keep(inputBuffer.toString());
-          break;
-        }
-        if (
-          !history[key.name]()
-        ) {
-          break;
-        }
-
-        autocompletion.clearHint();
-        history.keep(
-          inputBuffer.replace(history.current, { clearOutput: true })
-        );
-        break;
-      }
-      case "backspace": {
-        autocompletion.clearHint();
-        const { autocomplete } = inputBuffer.removeChar();
-
-        if (autocomplete) {
-          autocompletion.findHint(inputBuffer.toString());
-        }
-        break;
-      }
-      default: {
-        const { autocomplete } = inputBuffer.appendChar(input);
-
-        if (
-          autocomplete &&
-          autocompletion.findHint(inputBuffer.toString(), { currentInput: input })
-        ) {
-          process.stdout.write(input);
-        }
-        break;
-      }
-    }
-  };
-}
-
-function normalizeKey(
-  key: Key
-): string {
-  if (key.ctrl && key.name === "c") {
-    return "ctrl+c";
-  }
-
-  return key.name;
 }
